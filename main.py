ed3fc3029452c635197fc44c0a10a96852222f5f
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-23 16:44:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -90,11 +90,22 @@ class AIChat:
     def __init__(self, role_description, api_key):
         self.role_description = role_description
         self.api_key = api_key
+
     async def chat(self, message):
-        messages = [
-            {"role": "system", "content": self.role_description},
-            {"role": "user", "content": message}
-        ]
+        # Enhanced system message to guide the AI's behavior
+        system_message = {
+            "role": "system",
+            "content": f"{self.role_description}. Your task is to engage in a complex dialogue about Python programming, including generating Python code snippets that solve specific problems."
+        }
+        
+        # User message with more context for better code generation
+        user_message = {
+            "role": "user",
+            "content": f"{message}. Please provide a Python code snippet that demonstrates your solution, and explain the logic behind it."
+        }
+
+        messages = [system_message, user_message]
+
         try:
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo-16k",
@@ -105,7 +116,8 @@ class AIChat:
             return response['choices'][0]['message']['content']
         except Exception as e:
             logging.error(f"OpenAI Error: {e}")
-            return None
+            return "An error occurred."
+
 
 async def main():
     config = load_or_create_config()
