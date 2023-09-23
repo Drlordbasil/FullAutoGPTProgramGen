be07569e71d4ff185d47fa50a7426125a2417723
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-23 18:24:34 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,7 @@
-
 @echo off
+pip freeze > installed.txt
+for /F "delims==" %%i in (installed.txt) do pip uninstall -y %%i
+del installed.txt
+pip install -r requirements.txt
 python MasterAI.py
 pause
