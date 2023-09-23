9bbdf1c3af8346ea2146bd32546fd78c98d4ca9f
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-23 18:14:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from contextlib import contextmanager
 import tkinter as tk
 from tkinter import scrolledtext, Button, Label, OptionMenu, StringVar
 import threading
-import chat
+import chat as AIChat
 logging.basicConfig(level=logging.INFO, filename='ai_chat.log')
 
 @contextmanager
@@ -86,34 +86,6 @@ class PluginManager:
                 code = f.read()
                 exec(code, globals())
 
-class AIChat:
-    def __init__(self, role_description, api_key):
-        self.role_description = role_description
-        self.api_key = api_key
-
-    async def chat(self, message):
-        system_message = {
-            "role": "system",
-            "content": f"{self.role_description}. Your task is to engage in a complex dialogue about Python programming, including generating Python code snippets that solve specific problems."
-        }
-        user_message = {
-            "role": "user",
-            "content": f"{message}. Please provide a Python code snippet that demonstrates your solution, and explain the logic behind it."
-        }
-
-        messages = [system_message, user_message]
-
-        try:
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo-16k",
-                max_tokens=8000,
-                messages=messages,
-                api_key=self.api_key
-            )
-            return response['choices'][0]['message']['content']
-        except Exception as e:
-            logging.error(f"OpenAI Error: {e}")
-            return "An error occurred."
 
 async def main():
     config = load_or_create_config()
