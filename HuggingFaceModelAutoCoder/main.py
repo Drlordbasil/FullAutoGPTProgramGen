2b25fda1da4c115c911eba84a318c6e13a5026e4
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-25 11:21:42 -0400

    Update main.py

--- HuggingFaceModelAutoCoder/main.py
+++ HuggingFaceModelAutoCoder/main.py
@@ -1,6 +1,7 @@
 from huggingface_model import initialize_model, generate_code
 from exe_generator import generate_exe
-
+from fine_tune_model import fine_tune_model
+from listener import Listener
 def main():
     model_name = "gpt-neo-1.3B"  # Change to a suitable model
     tokenizer, model = initialize_model(model_name)
