43573536dde8a63c2725e8c0503a1358d94d8739
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-26 09:03:38 -0400

    Update chat.py

--- gpt4all/chat.py
+++ gpt4all/chat.py
@@ -1,1 +1,10 @@
+from gpt4all import GPT4ALL
 
+class Chat:
+    def __init__(self, model):
+        self.model = model
+
+    def gen(self, prompt):
+        response = self.model.generate(prompt, temp=0.3, max_tokens=300)
+        if response is not None:
+            return response
